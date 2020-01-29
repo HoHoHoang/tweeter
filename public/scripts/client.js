4c868096004879e0d39f64112657941225c22588
HoHoHoang <hoangdinh90@gmail.com>
2020-01-29 05:26:31 +0000

    able to add new tweets now

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,8 +8,7 @@
 
 $(document).ready(function() {
   
-
-// Test / driver code (temporary). Eventually will get this from the server.
+  // Test / driver code (temporary). Eventually will get this from the server.
 const tweetData = {
   "user": {
     "name": "Newton",
@@ -23,11 +22,53 @@ const tweetData = {
 }
 
 
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+}
 
-const $tweet = tweetData;
+const createTweetElement = function(tweet) {
+  let $tweet = $('<article>').addClass('tweet');
+  // ...
+  return $tweet;
+}
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweetContainer').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+let tweet = `<div class="tweetBox tweetBoxHover">
+<header class="tweetHeader">
+  <img class="profilePicture" src="/images/profile-hex.png">
+  <span class="profileName">${tweetData.user.name}</span>
+  <span class="profileHandle">@Barney</span>
+</header>
+
+<p class="theTweet">Such Wow! Much Amazing! Great Job!</p>
+
+<hr>
+
+<footer>
+  <span class="daysFooter">10 days ago</span>
+  <span class="emojiFooter">emojis</span>
+</footer>
+</div>`
+
+  const $input = $('input');
+
+  $input.click((event) => {
+    console.log(event)
+
+
+    const $tweets = $('#tweetContainer')
+    const $addTweet = $(tweet)
+
+    // $addTweet.text("hello")
+
+    // $addTweet.text(`${tweetData}`)
+
+
+
+    $tweets.append($addTweet);
+
+  })
+
 
 })
